9f348d29ef3cfc8b2799c55a274eb0c60dc5bd4c
azam21918 <khaa21918@gmail.com>
2025-06-29 13:09:16 +0600

    adding some javascript file

---
+++ js/login.js
@@ -0,0 +1,19 @@
+document.getElementById('btn-login')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const phoneNumber = document.getElementById('phone-number').value;
+        const pin = document.getElementById('pin').value;
+        const convertedPin = parseInt(pin)
+        if (phoneNumber.length == 11) {
+            if (convertedPin === 1234) {
+                alert('Welcome To The New Home Page');
+                window.location.href = "./main.html";
+            }
+            else {
+                alert('pin thick nai')
+            }
+        }
+        else {
+            alert('number thick nai');
+        }
+    })
